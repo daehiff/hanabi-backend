cc7c447bc26db59964c462555a63b792213cb25c
Travis CI <travis@travis-ci.org>
2020-07-09 14:34:11 +0200

    Travis build:

--- static/doc/api_project.js
+++ static/doc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-07-09T12:33:24.083Z",
+    "time": "2020-07-09T12:34:11.074Z",
     "url": "http://apidocjs.com",
     "version": "0.23.0"
   }
