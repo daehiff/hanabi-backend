428ea434fffee9faee99dd6379051c16cc0e61b4
David Winderl <davidwinderl@Davids-MacBook-Pro.local>
2020-07-09 14:24:49 +0200

    change some states

--- .travis/genDoc.sh
+++ .travis/genDoc.sh
@@ -15,7 +15,6 @@ upload_files() {
   git checkout -b $TRAVIS_BRANCH 
   git add static/doc
   git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
-  git remote rm origin
   git remote add origin https://${GH_ACESS_TOKEN}@github.com/daehiff/hanabi-backend.git > /dev/null 2>&1
   git push origin $TRAVIS_BRANCH 
 }
