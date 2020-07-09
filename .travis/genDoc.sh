029d6d8c9c6dddbb0eb02c3ad785d751d95ff2a3
Travis CI <travis@travis-ci.org>
2020-07-09 14:42:36 +0200

    added username

--- .travis/genDoc.sh
+++ .travis/genDoc.sh
@@ -3,24 +3,23 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  git checkout -b $TRAVIS_BRANCH 
+  git pull origin $TRAVIS_BRANCH 
 }
 
 generate_doc() {
   npm install -g apidoc
-  cd $TRAVIS_BUILD_DIR
   make doc
 }
 
 upload_files() {
-  git remove rm
-  git checkout -b $TRAVIS_BRANCH 
   git add static/doc
   git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
-  git remote add origin https://${GH_ACESS_TOKEN}@github.com/daehiff/hanabi-backend.git > /dev/null 2>&1
-  git push origin $TRAVIS_BRANCH 
+  git remote add origin https://travis:${GH_ACESS_TOKEN}@github.com/daehiff/hanabi-backend.git > /dev/null 2>&1
+  git push origin $TRAVIS_BRANCH
 }
 
-
+cd $TRAVIS_BUILD_DIR
 setup_git
 generate_doc
 upload_files
