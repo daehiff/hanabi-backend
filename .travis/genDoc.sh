79392bebea71d3a2cfc301122f55830fe2051a9d
David Winderl <davidwinderl@Davids-MacBook-Pro.local>
2020-07-09 14:21:47 +0200

    doing something stupid

--- .travis/genDoc.sh
+++ .travis/genDoc.sh
@@ -17,7 +17,7 @@ upload_files() {
   git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
   git remote rm origin
   git remote add origin https://${GH_ACESS_TOKEN}@github.com/daehiff/hanabi-backend.git > /dev/null 2>&1
-  git push origin $TRAVIS_BRANCH --quiet
+  git push origin $TRAVIS_BRANCH 
 }
 
 
