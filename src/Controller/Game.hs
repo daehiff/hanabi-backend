8d7c969142222201bd9d78b2ccc5bcedb7db7ab1
ru59tew <annika.kilian@campus.lmu.de>
2020-08-15 12:11:27 +0200

    properly formatted

--- src/Controller/Game.hs
+++ src/Controller/Game.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 
-module Controller.Game where -- no control
+module Controller.Game where
 
 import           Control.Monad.Trans            ( liftIO )
 import           Model
@@ -25,7 +25,12 @@ createDrawPile isRainbow = do
         )
   let numbers = [1, 1, 1, 2, 2, 3, 3, 4, 4, 5]
   let cards =
-        [ Card { cid = NewKey, color = color, number = number, hintColor = [], hintNumber = Nothing }
+        [ Card { cid        = NewKey
+               , color      = color
+               , number     = number
+               , hintColor  = []
+               , hintNumber = Nothing
+               }
         | color  <- colors
         , number <- numbers
         ]
@@ -33,13 +38,18 @@ createDrawPile isRainbow = do
 
 
 distributeCards :: [Player] -> [String] -> ([Player], [String])
-distributeCards players cardIDs = do
-  let amtCards = if length (players) < 4 then 5 else 4
-      restCards = drop (amtCards * length (players)) cardIDs
-      cardsToDistribute = chunksOf amtCards $ take (amtCards *length(players)) cardIDs
-      newPlayers = zipWith (\player cards -> player{cards = cards}) players cardsToDistribute
-      in  (newPlayers, restCards)
-  
+distributeCards players cardIDs =
+  let
+    amtCards  = if length (players) < 4 then 5 else 4
+    restCards = drop (amtCards * length (players)) cardIDs
+    cardsToDistribute =
+      chunksOf amtCards $ take (amtCards * length (players)) cardIDs
+    newPlayers = zipWith (\player cards -> player { cards = cards })
+                         players
+                         cardsToDistribute
+  in
+    (newPlayers, restCards)
+
 
 
 createGame :: [User] -> Settings -> AppHandle (HVect xs) Game
@@ -47,13 +57,16 @@ createGame users settings = do
   drawPile <- (liftIO $ createDrawPile (isRainbow settings))
     >>= \pile -> forM pile insertObject
   let drawPileIDs = [ _cid | (Key _cid) <- map cid drawPile ]
-  let players = [ Player {playerId = let (Key _uid) = uid user in _uid
-             , name = username user
-             , cards               = []
-             , explicitHints       = []
-             }| user <- users]
+  let players =
+        [ Player { playerId      = let (Key _uid) = uid user in _uid
+                 , name          = username user
+                 , cards         = []
+                 , explicitHints = []
+                 }
+        | user <- users
+        ]
   let (newPlayers, restCards) = distributeCards players drawPileIDs
-  let ( _uid) = playerId $ newPlayers !! 0
+  let (_uid)                  = playerId $ newPlayers !! 0
   let game = Game { gid           = NewKey
                   , currentPlayer = _uid
                   , players       = newPlayers
@@ -71,16 +84,21 @@ createGame users settings = do
   insertObject game
 
 
-giveHint:: (Either Color Int) -> String -> Game -> AppHandle (HVect xs) Game
-giveHint either id game = do
+giveHint :: (Either Color Int) -> String -> Game -> AppHandle (HVect xs) Game
+giveHint hint id game = do
   let (Just player) = find (\player -> playerId player == id) ((players game))
   (cardObjects :: [Maybe Card]) <- forM (cards player) findById
-  let cards = [card | (Just card) <- cardObjects]
-  let modifiedCards = map (updateCard either) cards
+  let cards         = [ card | (Just card) <- cardObjects ]
+  let modifiedCards = map (updateCard hint) cards
   forM modifiedCards updateObject
   return game
 
 
-updateCard:: (Either Color Int) -> Card -> Card
-updateCard (Left hcolor) card = if (color card) == hcolor || (color card) == Rainbow then card{hintColor = [hcolor]} else card
-updateCard (Right hnumber) card = if (number card) == hnumber then card{hintNumber = Just hnumber} else card
+updateCard :: (Either Color Int) -> Card -> Card
+updateCard (Left hcolor) card =
+  if (color card) == hcolor || (color card) == Rainbow
+    then card { hintColor = [hcolor] }
+    else card
+updateCard (Right hnumber) card = if (number card) == hnumber
+  then card { hintNumber = Just hnumber }
+  else card
