c7859bc7046f72947291f5e936375139d71acf5a
ru59tew <annika.kilian@campus.lmu.de>
2020-09-12 12:49:44 +0200

    richtige Routenplatzierung in Init.hs

--- src/Init.hs
+++ src/Init.hs
@@ -115,12 +115,12 @@ app = do
       post ("/lobby" <//> var <//> "kick" <//> var) $ kickPlayer
       get ("/lobby" <//> var <//> "status") $ getStatus
       post ("/lobby" <//> var <//> "launch") $ launchGame
+      post ("/lobby" <//> var <//> "settings") $ adjustSettings
+      post ("/lobby" <//> var <//> "remove") $ removeLobby
       
       -- Chat Routes
       post ("/chat/" <//> var <//> "send") $ handleSendMessage
       get ("/chat/" <//> var <//> "status") $ getChatStatus
-      post ("/lobby" <//> var <//> "settings") $ adjustSettings
-      post ("/lobby" <//> var <//> "remove") $ removeLobby
       -- Game Routes
       get ("game" <//> var <//> "status") $ getGameStatus
       post ("game" <//> var <//> "move") $ makeMove
