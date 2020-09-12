475a680d64c9d0470d535de709e991a0b55c36f6
daehiff <winderl13@gmail.com>
2020-09-12 13:54:21 +0200

    made doc better

--- haskell.js
+++ haskell.js
@@ -1,9 +1,7 @@
 /**
- * Haskell 
+ * Haskell comments detection for ApiDoc 
  */
 module.exports = {
-  // find document blocks between '###' and '###'
   docBlocksRegExp: /{-?(.+?)\uffff?(?:\s*)?-}/g,
-  // remove not needed tabs at the beginning
   inlineRegExp: /^(\t*)?/gm
 };  
